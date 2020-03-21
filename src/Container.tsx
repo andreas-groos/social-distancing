1aaa961785e3e169c6548cf7ee21b551a1ee9cc3
Andreas <andreas.groos1@gmail.com>
2020-03-21 09:08:35 -0700

    Simple circle drawn

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,13 +1,37 @@
-import React, { ReactElement } from 'react'
+import React, { useEffect, useRef, ReactElement } from 'react'
+import { select } from 'd3'
+import { useWindowSize } from './utils';
+
+const INTERVAL = 50;
 
 interface Props {
 
 }
 
 export default function Container({ }: Props): ReactElement {
-  return (
-    <div>
-      <h1>Container</h1>
-    </div>
-  )
+  const d3svg = useRef(null);
+  const { width, height } = useWindowSize();
+
+  console.log(width, height)
+  useEffect(() => {
+    if (d3svg.current) {
+      let svg = select(d3svg.current);
+      svg.append('circle').attr('cx', 10).attr('cy', 10).attr('r', 10).attr('fill', 'green')
+    }
+  }, []);
+
+  if (width && height) {
+    return (
+      <div>
+        <svg
+          className="bar-chart-container"
+          width={width - 10}
+          height={height - 10}
+          role="img"
+          ref={d3svg}
+        ></svg>
+      </div>
+    )
+  }
+  return <p>Loading</p>
 }
---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { WindowSize } from './types';
+
+export function degrees2Radians(degrees: number): number {
+  if (degrees > 360) {
+    degrees = degrees - 360;
+  }
+  return degrees * (Math.PI / 180);
+}
+
+
+export function useWindowSize(): WindowSize {
+  function getSize() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+  const [windowSize, setWindowSize] = useState(getSize);
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
