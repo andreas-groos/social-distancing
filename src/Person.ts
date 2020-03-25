566c34aebfc48de4d7ab2778883f6be6172ce5a3
Robert Diamond <rmd6502@roku.com>
2020-03-25 16:57:57 -0700

    add incubating and deceased statuses

--- src/Person.ts
+++ src/Person.ts
@@ -3,7 +3,6 @@ import { getRandomPosition } from './utils';
 import { select } from 'd3'
 
 const RADIUS = 10
-const HEALING_TIME = 5 * 1000;
 
 class Person implements IPerson {
   static count = 0
@@ -19,16 +18,20 @@ class Person implements IPerson {
   key: number
   status: Status
   infected: number | null
+  incubation_time: number | null
+  recovery_time: number | null
   constructor(width: number, height: number) {
+    this.key = Person.count
     this.x = getRandomPosition(width, RADIUS)
     this.y = getRandomPosition(height, RADIUS)
     this.r = RADIUS
     this.vx = (Math.random() * 2) - 1
     this.vy = (Math.random() * 2) - 1
     this.speed = 1
-    this.key = Person.count
     this.status = 'HEALTHY'
     this.infected = null // timestamp for infection or null
+    this.incubation_time = null;
+    this.recovery_time = null;
     Person.count++
     Person.window.height = height
     Person.window.width = width
@@ -47,20 +50,31 @@ class Person implements IPerson {
   }
 
   infection() {
-    this.status = 'SICK'
+    this.status = 'INCUBATING'
     this.infected = new Date().getTime()
-    Person.members[this.key] = 'SICK'
+    Person.members[this.key] = 'INCUBATING'
+    this.incubation_time = this.infected + (4 + Math.random() * 6) * 1000;
+    this.recovery_time = this.incubation_time + (4 + Math.random() * 6) * 1000;
   }
 
   checkStatus() {
     if (this.infected) {
-      const now = new Date().getTime()
-      let timeDifference = now - this.infected
-      if (timeDifference >= HEALING_TIME) {
-        this.infected = null
-        this.status = 'RECOVERED'
-        Person.members[this.key] = 'RECOVERED'
-      }
+        const now = new Date().getTime();
+        if (this.incubation_time && this.incubation_time <= now) {
+            this.incubation_time = null;
+            this.status = 'SICK';
+            this.speed = 0.5;
+            Person.members[this.key] = this.status;
+        } else if (this.recovery_time && this.recovery_time <= now) {
+            this.status = Math.random() < 0.02 ?  'DECEASED' : 'RECOVERED';
+            this.recovery_time = null;
+            if (this.status === 'DECEASED') {
+                this.speed = 0;
+            } else {
+                this.speed = 1;
+            }
+            Person.members[this.key] = this.status;
+        }
     }
   }
 
@@ -87,10 +101,14 @@ class Person implements IPerson {
     switch (this.status) {
       case 'HEALTHY':
         return '#6495ED'
+      case 'INCUBATING':
+        return '#AC743C'
       case 'SICK':
         return '#DC143C'
       case 'RECOVERED':
         return '#66CDAA'
+      case 'DECEASED':
+        return '#222222'
       default:
         return 'black'
     }
@@ -101,9 +119,14 @@ class Person implements IPerson {
     let healthy = 0
     let sick = 0
     let recovered = 0
+    let incubating = 0
+    let deceased = 0
     while (i < this.count) {
       if (Person.members[i] === 'HEALTHY') {
         healthy++
+      }
+      if (Person.members[i] === 'INCUBATING') {
+        incubating++
       }
       if (Person.members[i] === 'SICK') {
         sick++
@@ -111,12 +134,17 @@ class Person implements IPerson {
       if (Person.members[i] === 'RECOVERED') {
         recovered++
       }
+      if (Person.members[i] === 'DECEASED') {
+        deceased++
+      }
       i++
     }
     return {
       healthy,
+      incubating,
       sick,
-      recovered
+      recovered,
+      deceased
     }
   }
 }
--- src/Stats.tsx
+++ src/Stats.tsx
@@ -9,8 +9,10 @@ export default function StatsComponent({ stats }: Props): ReactElement {
   return (
     <div id="stats">
       <p>Healthy: {stats.healthy}</p>
+      <p>Incubating: {stats.incubating}</p>
       <p>Sick: {stats.sick}</p>
       <p>Recovered: {stats.recovered}</p>
+      <p>Deceased: {stats.deceased}</p>
 
     </div>
   )
